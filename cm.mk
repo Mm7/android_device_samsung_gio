20c3f68e33ad0b7bf7acb9aa0d2f5303e3b64623
erikcas <erikcas1972@gmail.com>
2013-06-16 23:33:57 +0200

    reorganize

--- cm.mk
+++ cm.mk
@@ -1,20 +1,19 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# BootAnimation
+TARGET_BOOTANIMATION_NAME := vertical-320x480
 
-# Inherit some common CM stuff.
+# Inherit device configuration for Gio
+$(call inherit-product, device/samsung/gio/full_gio.mk)
+
+# Inherit some common cyanogenmod stuff.
 $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/samsung/gio/full_gio.mk)
+# Include GSM stuff
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := GalaxyGio
 PRODUCT_VERSION_DEVICE_SPECIFIC := -GT-S5660
--include vendor/cm/config/common_versions.mk
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gio
 PRODUCT_NAME := cm_gio
-
-## Bootanimation
-TARGET_BOOTANIMATION_NAME := vertical-320x480
